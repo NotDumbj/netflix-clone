2c1affd70be1409557e4c6d2084e2a7e6c4608b1
NotDumbj <145663898+NotDumbj@users.noreply.github.com>
2024-03-13 06:51:59 +0500

    idk what is this

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,13 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-      <h1 className = "text-2xl text-green-500">hello there</h1>
+
+      <section>
+        <div class = "navbar">
+          <nav>
+            <h1 className = "text-2xl text-green-500">hello there</h1>
+          </nav>
+        </div>
+      </section>
   );
 }
